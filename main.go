01dd5dc4a40213dc21c7c42eaf174057388c0b86
AYGA2K <garouatayoub@gmail.com>
2025-11-16 21:06:15 +0400

    simplify code

--- main.go
+++ main.go
@@ -48,88 +48,93 @@ func main() {
 			os.Exit(1)
 		}
 		clients[con] = &types.Client{}
-		go handler(con, m, mlist, streams, listMutex, streamsMutex, waitingClients, clients, *replicaof, masterReplid, replicas)
-	}
-}
 
-func handler(con net.Conn, m map[string]types.SetArg, mlist map[string][]string, streams map[string][]types.StreamEntry, listMutex, streamsMutex *sync.Mutex, waitingClients map[string][]chan any, clients map[net.Conn]*types.Client, replicaof string, masterReplid string, replicas *types.ReplicaConns) {
-	defer con.Close()
-	reader := bufio.NewReader(con)
-	client := clients[con]
-	for {
-		commands, err := parser.ParseRespArray(reader)
-		if err != nil {
-			fmt.Println("Error parsing:", err.Error())
-			return
-		}
-		if len(commands) == 0 {
-			continue
-		}
-		if client.InMulti && (strings.ToUpper(commands[0]) != "EXEC" && strings.ToUpper(commands[0]) != "DISCARD") {
-			client.Commands = append(client.Commands, commands)
-			con.Write([]byte("+QUEUED\r\n"))
-			continue
-		}
-		var response string
-		switch strings.ToUpper(commands[0]) {
-		case "PING":
-			response = handlers.Ping()
-		case "INFO":
-			response = handlers.Info(commands, replicaof, masterReplid)
-		case "ECHO":
-			response = handlers.Echo(commands)
-		case "SET":
-			response = handlers.Set(commands, m, replicas)
-		case "INCR":
-			response = handlers.Incr(commands, m, replicas)
-		case "GET":
-			response = handlers.Get(commands, m)
-		case "RPUSH":
-			response = handlers.Rpush(commands, mlist, listMutex, waitingClients, replicas)
-		case "LPUSH":
-			response = handlers.Lpush(commands, mlist, listMutex, replicas)
-		case "LRANGE":
-			response = handlers.Lrange(commands, mlist, listMutex)
-		case "LLEN":
-			response = handlers.Llen(commands, mlist, listMutex)
-		case "LPOP":
-			response = handlers.Lpop(commands, mlist, listMutex, replicas)
-		case "BLPOP":
-			response = handlers.Blpop(commands, mlist, listMutex, waitingClients)
-		case "TYPE":
-			response = handlers.Type(commands, m, mlist, streams)
-		case "XADD":
-			response = handlers.Xadd(commands, streams, streamsMutex, waitingClients, replicas)
-		case "XRANGE":
-			response = handlers.Xrange(commands, streams, streamsMutex)
-		case "XREAD":
-			response = handlers.Xread(commands, streams, streamsMutex, waitingClients)
-		case "MULTI":
-			response = handlers.Multi(client)
-		case "EXEC":
-			if !client.InMulti {
-				response = "-ERR EXEC without MULTI\r\n"
-			} else if client.InMulti && len(client.Commands) > 0 {
-				handlers.Exec(con, clients, m, mlist, streams, listMutex, streamsMutex, waitingClients, replicas)
-			} else if client.InMulti && len(client.Commands) == 0 {
-				response = "*0\r\n"
-				client.InMulti = false
+		go func(con net.Conn) {
+			defer con.Close()
+			reader := bufio.NewReader(con)
+			client := clients[con]
+
+			for {
+				commands, err := parser.ParseRespArray(reader)
+				if err != nil {
+					fmt.Println("Error parsing:", err.Error())
+					return
+				}
+				if len(commands) == 0 {
+					continue
+				}
+
+				if client.InMulti && (strings.ToUpper(commands[0]) != "EXEC" && strings.ToUpper(commands[0]) != "DISCARD") {
+					client.Commands = append(client.Commands, commands)
+					con.Write([]byte("+QUEUED\r\n"))
+					continue
+				}
+
+				var response string
+				switch strings.ToUpper(commands[0]) {
+				case "PING":
+					response = handlers.Ping()
+				case "INFO":
+					response = handlers.Info(commands, *replicaof, masterReplid)
+				case "ECHO":
+					response = handlers.Echo(commands)
+				case "SET":
+					response = handlers.Set(commands, m, replicas)
+				case "INCR":
+					response = handlers.Incr(commands, m, replicas)
+				case "GET":
+					response = handlers.Get(commands, m)
+				case "RPUSH":
+					response = handlers.Rpush(commands, mlist, listMutex, waitingClients, replicas)
+				case "LPUSH":
+					response = handlers.Lpush(commands, mlist, listMutex, replicas)
+				case "LRANGE":
+					response = handlers.Lrange(commands, mlist, listMutex)
+				case "LLEN":
+					response = handlers.Llen(commands, mlist, listMutex)
+				case "LPOP":
+					response = handlers.Lpop(commands, mlist, listMutex, replicas)
+				case "BLPOP":
+					response = handlers.Blpop(commands, mlist, listMutex, waitingClients)
+				case "TYPE":
+					response = handlers.Type(commands, m, mlist, streams)
+				case "XADD":
+					response = handlers.Xadd(commands, streams, streamsMutex, waitingClients, replicas)
+				case "XRANGE":
+					response = handlers.Xrange(commands, streams, streamsMutex)
+				case "XREAD":
+					response = handlers.Xread(commands, streams, streamsMutex, waitingClients)
+				case "MULTI":
+					response = handlers.Multi(client)
+				case "EXEC":
+					if !client.InMulti {
+						response = "-ERR EXEC without MULTI\r\n"
+					} else if len(client.Commands) > 0 {
+						handlers.Exec(con, clients, m, mlist, streams, listMutex, streamsMutex, waitingClients, replicas)
+					} else {
+						response = "*0\r\n"
+						client.InMulti = false
+					}
+				case "DISCARD":
+					if !client.InMulti {
+						response = "-ERR DISCARD without MULTI\r\n"
+					} else {
+						client.InMulti = false
+						client.Commands = nil
+						response = "+OK\r\n"
+					}
+				case "REPLCONF":
+					response = handlers.Replconf()
+				case "PSYNC":
+					handlers.Psync(con, masterReplid, replicas)
+				default:
+					response = "-ERR unknown command\r\n"
+				}
+
+				if response != "" {
+					con.Write([]byte(response))
+				}
 			}
-		case "DISCARD":
-			response = handlers.Discard(client)
-		case "REPLCONF":
-			response = handlers.Replconf()
-		case "PSYNC":
-			handlers.Psync(con, masterReplid, replicas)
-		default:
-			response = " unknown command\r\n"
-		}
-		if response != "" {
-			writeResponse(con, response)
-		}
+		}(con)
 	}
-}
-
-func writeResponse(con net.Conn, response string) {
-	con.Write([]byte(response))
 }
